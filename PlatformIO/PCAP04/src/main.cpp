4d4032081f8f031d93ccdeda9d06fcc25b2e957b
Tom Salden <tomsalden@live.nl>
2022-09-07 10:19:18 +0200

    Multiple configurations allowed

--- PlatformIO/PCAP04/src/main.cpp
+++ PlatformIO/PCAP04/src/main.cpp
@@ -75,18 +75,25 @@ void setup() {
   if (pcap1_enable == true){
     Serial.println("Initializing 1st PCAP");
     initialisePCAP(&pcap1, &Config_PCAP_1, pcap1_i2c, pcap1_addr);
-    attachInterrupt(digitalPinToInterrupt(pcap1_int),pcap_cdc_complete_callback,FALLING);
+    attachInterrupt(digitalPinToInterrupt(pcap1_int),pcap1_cdc_complete_callback,FALLING);
     Serial.println("1st PCAP04 has been connected and is initialised");
+    ESPUI.updateLabel(webserverIDs.STATUS,"1st PCAP04 has been initialised");
   }
 
   if (pcap2_enable == true){
     Serial.println("Initializing 2nd PCAP");
     initialisePCAP(&pcap2, &Config_PCAP_2, pcap2_i2c, pcap2_addr);
+    attachInterrupt(digitalPinToInterrupt(pcap2_int),pcap2_cdc_complete_callback,FALLING);
+    Serial.println("2nd PCAP04 has been connected and is initialised");
+    ESPUI.updateLabel(webserverIDs.STATUS,"2nd PCAP04 has been initialised");
   }
 
   if (pcap3_enable == true){
     Serial.println("Initializing 3rd PCAP");
     initialisePCAP(&pcap3, &Config_PCAP_3, pcap3_i2c, pcap3_addr);
+    attachInterrupt(digitalPinToInterrupt(pcap3_int),pcap3_cdc_complete_callback,FALLING);
+    Serial.println("3rd PCAP04 has been connected and is initialised");
+    ESPUI.updateLabel(webserverIDs.STATUS,"3rd PCAP04 has been initialised");
   }
 
   
@@ -94,15 +101,29 @@ void setup() {
   digitalWrite(ledG, HIGH);
   delay(1000);
 
-  pcap1.cdc_complete_flag = true; //Start the first readout. Then the chip continues
-  ESPUI.updateLabel(webserverIDs.STATUS,"Initialized - Started measurements");
+  //Start the first readout. Then the chip continues
+  pcap1.cdc_complete_flag = true; 
+  pcap2.cdc_complete_flag = true;
+  pcap3.cdc_complete_flag = true;
+
+  ESPUI.updateLabel(webserverIDs.STATUS,"Initialized - Loading correct config");
 
   //Read and apply the configuration from the SD card
-  readConfigfromSD(config1,&Config_PCAP_1);
-  pcap1.update_config(&Config_PCAP_1);
-  //Serial.println("current config");
-  //pcap1.print_config();
+  if (pcap1_enable == true){
+    readConfigfromSD(config1,&Config_PCAP_1);
+    pcap1.update_config(&Config_PCAP_1);
+  }
+  if (pcap2_enable == true){
+    readConfigfromSD(config2,&Config_PCAP_2);
+    pcap2.update_config(&Config_PCAP_2);
+  }
+  if (pcap3_enable == true){
+    readConfigfromSD(config3,&Config_PCAP_3);
+    pcap3.update_config(&Config_PCAP_3);
+  }
   updateFromConfig();
+
+  ESPUI.updateLabel(webserverIDs.STATUS,"Measurements active");
 }
 
 
